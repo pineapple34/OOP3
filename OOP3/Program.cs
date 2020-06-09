ff0819b3b71d475ad921ef7c39fca5f4f1a83125
pineapple34 <pineapple6934@gmail.com>
2020-06-09 15:25:07 +0500

    Initial commit

---
+++ OOP3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Book
+    {
+        public string author, title;
+        private int pageCount;
+        public bool collection;
+
+        public Book()
+        {
+            this.author = "Л.Н. Толстой";
+            this.title = "Война и мир";
+            this.pageCount = 100500;
+            this.collection = true;
+        }
+        public Book(string author, string title, int pageCount, bool collection)
+        {
+            this.author = author;
+            this.title = title;
+            this.pageCount = pageCount;
+            this.collection = collection;
+        }
+        public Book(string author, string title, int pageCount)
+        {
+            this.author = author;
+            this.title = title;
+            this.pageCount = pageCount;
+            this.collection = false;
+        }
+    }
+}
